6657031830385e6e4b6ee4a08936c237072b03c2
Dmitriy Timofeev <Develop1@masproject.pro>
2019-06-26 18:56:36 +0300

    Поправил шаблонизатор

--- build/template-engine.js
+++ build/template-engine.js
@@ -1,2 +1,8 @@
 const TemplateEngine = require('../src/TemplateEngine/TemplateEngine.js');
-module.exports = TemplateEngine;
+/**
+ * @param  {object} obj — Структура блоков интерфейса в формате BEMJSON
+ * @return {string} HTML разметка страницы
+ */
+export default function (obj) {
+    return TemplateEngine(obj);
+}
